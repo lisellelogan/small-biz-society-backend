c6225f2ece63c440c062e152d214b83ce53fc930
lisellelogan <liselle.logan@yahoo.com>
2021-04-22 11:41:14 -0400

    change name of small_business route to plural form

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :small_business_items
-  resources :small_business
+  resources :small_businesses
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

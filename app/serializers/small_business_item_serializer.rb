16b10a6b2ac9be9762edfdad2efefac2ab590ba8
lisellelogan <liselle.logan@yahoo.com>
2021-04-22 12:32:23 -0400

    remove belongs_to

--- app/serializers/small_business_item_serializer.rb
+++ app/serializers/small_business_item_serializer.rb
@@ -1,5 +1,4 @@
 class SmallBusinessItemSerializer
   include FastJsonapi::ObjectSerializer
-  belongs_to :small_business
   attributes :name, :price, :description, :image_url, :type_of_product, :small_business_id
 end

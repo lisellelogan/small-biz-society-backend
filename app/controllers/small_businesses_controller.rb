13d0219e9e81b460886e255e982990cf77b8405a
lisellelogan <liselle.logan@yahoo.com>
2021-04-22 12:40:07 -0400

    create show action

--- app/controllers/small_businesses_controller.rb
+++ app/controllers/small_businesses_controller.rb
@@ -3,4 +3,9 @@ class SmallBusinessesController < ApplicationController
         small_businesses = SmallBusiness.all
         render json: SmallBusinessSerializer.new(small_businesses, include: [:small_business_items])
     end
+
+    def show 
+        small_business = SmallBusiness.find_by(params[:id])
+        render json: SmallBusinessSerializer.new(small_business)
+    end
 end

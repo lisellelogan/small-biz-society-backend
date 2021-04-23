2ad40dd8b648d376cff4cea7be1084478e94bfe5
lisellelogan <liselle.logan@yahoo.com>
2021-04-23 13:17:22 -0400

    change type to type_of_product

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,5 +22,5 @@ the_coffee_spot.small_business_items.create([
 trash_to_treasure.small_business_items.create([
     {name: "Classy Not Trashy", price: 79.99, description: "Handcrafted earrings that everyone needs in their jewlery box.", image_url: "https://images.unsplash.com/photo-1611271511360-f9385e4a45bf?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80", type_of_product: "Jewelry"},
     {name: "Layer Me Up", price: 119.99, description: "A set of 3 necklaces that layer perfectly for that effortlessly chic look.", image_url: "https://images.unsplash.com/photo-1608543837910-b73953ea3417?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80", type_of_product: "Jewelry"},
-    {name: "Ringin'", price: 78.50, description: "A gold ring set that will blow Thanos out of this universe.", image_url: "https://images.unsplash.com/photo-1608042314453-ae338d80c427?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1040&q=80", type: "Jewelry"}
+    {name: "Ringin'", price: 78.50, description: "A gold ring set that will blow Thanos out of this universe.", image_url: "https://images.unsplash.com/photo-1608042314453-ae338d80c427?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1040&q=80", type_of_product: "Jewelry"}
 ])

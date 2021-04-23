890b101f1ab88732a3fb077a06fa2bf49528a0b1
lisellelogan <liselle.logan@yahoo.com>
2021-04-22 21:39:41 -0400

    update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,7 @@ SmallBusinessItem.destroy_all
 
 #create small businesses
 the_coffee_spot = SmallBusiness.create(name: "The Coffee Spot", price_range: "$", address: "123 Coffee Street, New York, NY 10023")
-candle_collective = SmallBusiness.create(name: "Candle Collective", price_range: "$$", address: "456 Candle Road, Brooklyn, NY 10011")
+trash_to_treasure = SmallBusiness.create(name: "Trash To Treasure", price_range: "$$", address: "789 Gold Street, New York, NY 10003")
 
 #create small business items
 the_coffee_spot.small_business_items.create([
@@ -19,8 +19,8 @@ the_coffee_spot.small_business_items.create([
     {name: "Drip Coffee", price: 3.75, description: "Hot water poured over our freshly ground coffee.", image_url: "add drip coffee image here", type_of_product: "Coffee & Tea"}
 ])
 
-candle_collective.small_business_items.create([
-    {name: "Bahama Breeze", price: 30, description: "Handmade candle that gives you the vacation ambiance!", image_url: "add bahama breeze image here", type_of_product: "Home"},
-    {name: "Fresh Linen", price: 25, description: "Handmade candle that smells like you just did laundry (Even if you didn't).", image_url: "add fresh linen image here", type_of_product: "Home"}
-    (name: "Une Cafe", price: 40, description: "Handmade candle that smells like your favorite coffee shop in the comfort of your own home!", image_url: "add une cafe image here", type_of_product: "Home")
+trash_to_treasure.small_business_items.create([
+    {name: "Classy Not Trashy", price: 79.99, description: "Handcrafted earrings that everyone needs in their jewlery box.", image_url: "add earrings here", type_of_product: "Jewelry"},
+    {name: "Layer Me Up", price: 119.99, description: "A set of 3 necklaces that layer perfectly for that effortlessly chic look.", image_url: "add necklace here", type_of_product: "Jewelry"},
+    {name: "Ringin'", price: 78.50, description: "A gold ring set that will blow Thanos out of this universe.", image_url: "add rings here", type: "Jewelry"}
 ])
